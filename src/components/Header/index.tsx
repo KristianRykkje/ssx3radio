9b170384128991cc0a52d87303f170813a37eb1e
KristianRykkje <kriryk@gmail.com>
2022-10-31 20:49:08 +0100

    wip

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,8 +5,8 @@ export const Header = () => {
   return (
     <header>
       <Head>
-        <title>SSX3 Radio Big</title>
-        <meta name="description" content="SSX3 Radio big" />
+        <title>SSX 3 Radio Big</title>
+        <meta name="description" content="SSX 3 Radio big" />
       </Head>
       <h1>SSX3 Radio</h1>
     </header>
