f9d3a2176a2f93fd16e709c5aabad96b579da487
KristianRykkje <kriryk@gmail.com>
2022-10-31 20:47:54 +0100

    update head

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import React from "react";
 
 export const Header = () => {
   return (
     <header>
+      <Head>
+        <title>SSX3 Radio Big</title>
+        <meta name="description" content="SSX3 Radio big" />
+      </Head>
       <h1>SSX3 Radio</h1>
     </header>
   );
